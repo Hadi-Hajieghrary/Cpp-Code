e759bf18d81b50824b1ec56b751d7db99d19a033
Hadi Hajieghrary <Hadi.Hajieghrary@Gmail.Com>
2021-04-26 10:57:04 -0400

    The drawing is seperated into several functions

--- drawing_fractal/main.cpp
+++ drawing_fractal/main.cpp
@@ -2,34 +2,47 @@
 #include <iostream>
 #include <cmath>
 
+
 #include "include/bitmap.hpp"
 
 using namespace std;
 
-void DrawMandelBrot(const char* file_name, int width, int height, int cX = 0, int cY = 0, double scale = 1, int colorOpt = 1, int max_iteration = 1000){
-    
-    Bitmap canvas(width,height);
-    
-    int* historgram = new int[max_iteration+1]{0};
-    int* fractal = new int[width*height]{0};
-    
-    
-    for (int X = 0; X < width; X++){
-        for (int Y = 0; Y < height; Y++){
-            //double x = (2*X-width)/2 * 2.0 /width;
-            //double y = (2*Y-height)/2 * 2.0 /height;
-            
-            double x = ( (2*X-width)/2 + cX ) / scale * 2.0/width;
-            double y = ( (2*Y-height)/2 + cY) / scale * 2.0/height;
-            
-            
+
+struct Point{
+    int x;
+    int y;
+};
+
+struct Box{
+    Point corner;
+    int width;
+    int height;
+};
+
+struct MandelBrotFractal{
+    const int width;
+    const int height;
+    const unsigned int max_iteration;
+    int* const historgram;
+    int* const image;
+    const double scale;
+    const Point center;
+};
+
+void DrawMandelBrot(MandelBrotFractal fractal, Box box){
+
+    for (int x = box.corner.x; x < box.corner.x + box.width; x++){
+        for (int y = box.corner.y; y < box.corner.y + box.height; y++){
+            double x_bias = ( (2*x - static_cast<double>(fractal.width))/2.0 + fractal.center.x ) 
+                                / fractal.scale * 2.0/static_cast<double>(fractal.width);
+            double y_bias = ( (2*y - static_cast<double>(fractal.height))/2.0 + fractal.center.y ) 
+                                / fractal.scale * 2.0/static_cast<double>(fractal.height);
             double zx = 0;
             double zy = 0;
-            
             int itr = 0;
-            while(itr!=max_iteration){
-                double temp_zx = zx*zx - zy*zy + x;
-                double temp_zy = 2.0*zx*zy + y;
+            while(itr!=fractal.max_iteration){
+                double temp_zx = zx*zx - zy*zy + x_bias;
+                double temp_zy = 2.0*zx*zy + y_bias;
                 if(sqrt(temp_zx*temp_zx + temp_zy*temp_zy) > 2){
                     break;
                 }
@@ -37,49 +50,85 @@ void DrawMandelBrot(const char* file_name, int width, int height, int cX = 0, in
                 zy = temp_zy;
                 itr++;
             }
-            fractal[Y*width+X] = itr;
-            historgram[itr]++;
+            fractal.image[y*fractal.width+x] = itr;
+            fractal.historgram[itr]++;
         }
     }
-    
-    int total = 0;
-    for(int i=0; i<max_iteration; i++){
-        total = total + historgram[i];
-    }
-    
-    for (int X = 0; X<width; X++){
-        for (int Y = 0; Y<height; Y++){
-            
-            uint8_t red = 0;
-            uint8_t green = 0;
-            uint8_t blue = 0;
-            double alpha = 0;
-            for(int i=0; i<= fractal[Y*width+X]; i++){
-                alpha += static_cast<double>(historgram[i])/total;
-            }
-            switch (colorOpt) {
-                case 1:
-                    green = alpha * 255;
-                    break;
-                case 2:
-                    green = pow(255, alpha);
-                    break;
-            }
-            canvas.setPixel(X, Y, red, green, blue );
-        }
-    }
-    
-    delete[] historgram;
-    delete[] fractal;
-    canvas.save(file_name);
 
 }
 
+enum class Color{red, green, blue};
 
+void saveToImage(MandelBrotFractal m_b_fractal, char* file_name, Color color, int color_option = 1){
+
+    Bitmap* bmp = new Bitmap(m_b_fractal.width,m_b_fractal.height);
+
+    int total = 0;
+    for(int i=0; i<m_b_fractal.max_iteration; i++){
+        total = total + m_b_fractal.historgram[i];
+    }
+    for (int x = 0; x<m_b_fractal.width; x++){
+        for (int y = 0; y<m_b_fractal.height; y++){
+
+            uint8_t shade = 0;
+            double alpha = 0;
+            for(int i = 0; i<= m_b_fractal.image[y*m_b_fractal.width+x]; i++){
+                alpha += static_cast<double>(m_b_fractal.historgram[i])/total;
+            }
+            switch (color_option) {
+                case 1:
+                    shade = alpha * 255;
+                    break;
+                case 2:
+                    shade = pow(255, alpha);
+                    break;
+            }
+            switch (color)
+            {
+                case Color::red:
+                    bmp->setPixel(x, y, shade, 0, 0 );
+                    break;
+                case Color::green:
+                    bmp->setPixel(x, y, 0, shade, 0 );
+                    break;
+                case Color::blue:
+                    bmp->setPixel(x, y, 0, 0, shade );
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    }
+    bmp->save(file_name);
+}
 
 int main(int argc, const char * argv[]) {
-    
-    DrawMandelBrot("MandelBrot.bmp", 3840 , 2160, 0, 0, 1, 1);
 
+    const size_t width = 3840/5;
+    const size_t height = 2160/5;
+    const unsigned int max_iteration = 1000;
+    int* historgram = new int[max_iteration + 1]{0};
+    int* fractal = new int[width * height]{0};
+    const double scale = 1.0;
+
+    Point center{0, 0};
+    
+
+    MandelBrotFractal mandel_brot_fractal{width,height,max_iteration,
+                                            historgram, fractal, 
+                                            scale, center};
+    
+    DrawMandelBrot(mandel_brot_fractal, Box{Point{0,0}, 384, 216});
+    DrawMandelBrot(mandel_brot_fractal, Box{Point{1*384,0*216},384,216});
+    DrawMandelBrot(mandel_brot_fractal, Box{Point{0*384,1*216},384,216});
+    DrawMandelBrot(mandel_brot_fractal, Box{Point{1*384,1*216},384,216});
+    
+    char file_name[]{"MandelBrot.bmp"};
+    saveToImage(mandel_brot_fractal, file_name, Color::red);
+
+    delete[] historgram;
+    delete[] fractal;
     return 0;
+    
 }
